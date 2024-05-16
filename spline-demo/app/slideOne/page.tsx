8cd94dce0ad26370ef8a5c275555d434eae64af6
benjamin jouanno <benjamin.jouanno@ekino.com>
2024-05-16 17:36:36 +0200

    slide one

--- spline-demo/app/slideOne/page.tsx
+++ spline-demo/app/slideOne/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import Spline from '@splinetool/react-spline';
+
 export default function SlideOne() {
     return (
-      <div>
-        <h1>Slide One</h1>
+      <div className="bg">
+    <Spline scene="https://prod.spline.design/pXnT4EAN-GXNFPLl/scene.splinecode" />
       </div>
     );
   }
