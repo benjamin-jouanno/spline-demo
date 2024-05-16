f24745e1f47ed27e99c4fbfdd0d5a9aa319477ff
benjamin jouanno <benjamin.jouanno@ekino.com>
2024-05-16 17:41:29 +0200

    link add

--- spline-demo/app/page.tsx
+++ spline-demo/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Spline from "@splinetool/react-spline";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <div className="content">
-        <h1 className="title">SPLINE X HUGE FRONT</h1>
+        <h1 className="title">SPLINE X HUGE FRONT <p className="start"><Link href="/slideOne">{'Start ->'}</Link></p></h1>
       </div>
       <div className="bg">
         <Spline scene="https://prod.spline.design/QF1J3Lj-IDFqPrQY/scene.splinecode" />
